5f959b3f5aa1985901d0e48b8b3e5496382bdfc3
Birgen <birgen156@gmail.com>
2025-04-16 11:54:19 +0300

    newpost

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-mysql.createConnection({
-  host: "local host",
-  port: 3386,
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
   user: "root",
   password: "",
   database: "blog_db",
@@ -29,18 +29,38 @@ app.get("/register", (req, res) => {
 app.get("/posts", (req, res) => {
   //show all posts with possible actions like(edit, delete, hide, view post comments, view likes and much more) - the action will be in form of links or buttons
   connection.query("SELECT * FROM posts", (error, posts) => {
-    console.log(posts);
+    if (error) {
+      res.json(error);
+    } else {
+      console.log(posts);
+      res.json(posts);
+    }
   });
-  res.render("posts.ejs");
+  //res.render("posts.ejs");
 });
 
 app.get("/posts/:postID", (req, res) => {
   //show a single post (all content,images, comments(preview))- for public(no actions),for admin(actions like edit delete to be shown on this page)
   res.render("post.ejs");
 });
+app.get("/newpost", (req, res) => {
+  res.render("newpost.ejs");
+});
 
-app.post("/", (req, res) => {
-  res.redirect("/posts/5");
+app.post("/newpost", express.urlencoded({ extended: true }), (req, res) => {
+  console.log(req.body);
+  connection.query(
+    `INSERT INTO posts (user_id,title,content)VALUES(2,'${req.body.title}','${req.body.content}')`,
+    (err) => {
+      if (err) {
+        res.status(504);
+        res.json(err);
+      } else {
+        res.redirect("/posts");
+      }
+    }
+  );
+  // res.redirect("/post/5");
 });
 
 //listen/start --always define/write after all routes ---at the end (js is interprated)
